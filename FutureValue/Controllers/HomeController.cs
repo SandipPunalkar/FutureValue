1ec38d07e7fc0c163ea49198c11d40ae984303fe
Sandip Punalkar <sandippunalkar9@gmail.com>
2022-01-28 09:04:01 +0530

    Add project files.

---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using FutureValue.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+      
+    }
+}
