152f36309cb2440db009bd641e4241891c849065
Sandip Punalkar <sandippunalkar9@gmail.com>
2022-01-28 09:05:27 +0530

    ViewBag exampal.

--- FutureValue/Controllers/HomeController.cs
+++ FutureValue/Controllers/HomeController.cs
@@ -15,7 +15,8 @@ namespace FutureValue.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-           
+            ViewBag.Name = "Mary";
+            ViewBag.FV = 99999.99;
             return View();
         }
 
